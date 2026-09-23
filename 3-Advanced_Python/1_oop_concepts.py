6c735dc2aaa68fd2a3cbff6f661e3fa76d8c1cdb
S P Monish <spmonish84@gmail.com>
2026-09-23 22:30:49 +0530

    Day 26 | 23-09-2026 |Practiced theory concepts on Object Oreinted Programming, Classes and Objects

---
+++ 3-Advanced_Python/1_oop_concepts.py
@@ -0,0 +1,17 @@
+# Python OOP Concepts
+# object oriented programming empowers developers to build modular, maintainable and scalable applications. OOP is a way of organizing
+# code that uses objects and classes to represent real-world entities and their behavior. In OOP objects has attributes thing that has specific
+# data and can perform certain actions using methods
+
+# 1. Organizes code into classes and objects
+# 2. Supports encapsulation to group data and methods together
+# 3. Enables inheritance for reusability and hierarchy
+# 4. Allows polymorphism for flexible method implementation
+
+
+# Class - A blueprint or template for creating objects
+# Object - An instance of a class
+# Encapsulation - Protects data from direct access
+# Abstraction - Hides unnecessary details
+# Polymorphism - Allows the same method to behave differently
+# Inheritance - reuse code from existing classes
